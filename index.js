e7283ff970753879fcb78845643a974a9991b215
ihsanalamsyah <ihsanalamsyah596@gmail.com>
2021-09-18 00:38:38 +0700

    challenge binar

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+
+// mengambil data dari models
 const { User } = require('./models');
 
 app.use(express.json());
+app.set('view engine', 'ejs');
 
+// dipakai untuk dapat membaca dari form
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+// read data user
 app.get('/users', (req, res) => {
     User.findAll()
     .then(user => {
@@ -11,6 +22,7 @@ app.get('/users', (req, res) => {
     })
 })
 
+// create data user
 app.post('/users', (req, res) => {
     User.create({
         ID: req.body.ID,
@@ -25,4 +37,34 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users/delete/:id', (req, res) => {
+    User.destroy( { where: { id: req.params.id }})
+    .then(() => {
+        res.send(`Data user dengan id ${req.params.id} berhasil di hapus`)
+    })
+})
+
+// buat API untuk mengerah ke endpoint /users/update/:id
+app.get('/users/update/:id', (req, res) => {
+    User.findOne( { where: { id: req.params.id }})
+    .then((user) => {
+        res.render('/users/update', { user })
+    })
+})
+
+app.post('/users/update/:id', (req, res) => {
+    User.update({
+        username: req.body.username,
+        password: req.body.password
+    },
+    {
+        where: { id: req.params.id }
+    })
+    .then(() => {
+        res.send('User berhasil di ubah')
+    })
+})
+
 app.listen(3000);
+
+// associate: cara mengambil data sama kyk join di mysql
